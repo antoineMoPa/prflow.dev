316bbd70954262ecb69caf0bede9459c9201f661
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2024-07-31 10:42:36 -0400

    Some cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import { HydrateClient } from "../trpc/server";
 import NavBar from "./_components/NavBar";
 
 export default async function Home() {
-    const session = await getServerAuthSession();
-
     return (
         <HydrateClient>
             <NavBar/>
@@ -13,13 +11,6 @@ export default async function Home() {
                     <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem] text-center">
                         Measure & <span className="text-[hsl(280,100%,70%)]">improve</span> team workflows
                     </h1>
-                    <div className="flex flex-col items-center gap-2">
-                        <div className="flex flex-col items-center justify-center gap-4">
-                            <p className="text-center text-2xl text-white">
-                                {session && <span>Logged in as {session.user?.name}</span>}
-                            </p>
-                        </div>
-                    </div>
                 </div>
             </main>
         </HydrateClient>
