77ce5fa96865dc2874ceeb4d6734cf993f8864f8
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2024-08-02 01:12:12 -0400

    Begin using that api

--- src/server/api/routers/teamDashboard.ts
+++ src/server/api/routers/teamDashboard.ts
@@ -6,6 +6,37 @@ import {
 } from "../../../server/api/trpc";
 import { type Team } from "@prisma/client";
 
+import { Octokit } from "@octokit/rest";
+
+
+const getTeamStats = async ({
+    githubToken,
+    teamMembers,
+    githubRepositories,
+}: {
+    githubToken: string,
+    teamMembers: string[],
+    githubRepositories: string[],
+}) => {
+    const octokit = new Octokit({
+        auth: githubToken,
+    });
+    const pulls = [];
+
+    console.log(githubRepositories);
+
+    githubRepositories.forEach(async (repoPath) => {
+        const owner = repoPath.split("/")[0] as string;
+        const repo = repoPath.split("/")[1] as string;
+        const repoStats = await octokit.rest.pulls.list({
+            owner,
+            repo,
+            sort: "updated",
+        });
+
+        console.log(repoStats);
+    });
+};
 
 export const teamDashboardRouter = createTRPCRouter({
 
@@ -20,6 +51,8 @@ export const teamDashboardRouter = createTRPCRouter({
                     id: input.teamId,
                 },
             });
+
+            // TODO: allow a team member to view the dashboard
 
             if (!team) {
                 throw new Error("Team not found");
@@ -37,10 +70,28 @@ export const teamDashboardRouter = createTRPCRouter({
                 },
             });
 
+            const githubToken = await ctx.db.authToken.findFirst({
+                where: {
+                    teamId: input.teamId,
+                    type: "github",
+                },
+            });
+
+            if (!githubToken) {
+                throw new Error("GitHub token not found");
+            }
+
+            const stats = await getTeamStats({
+                githubToken: githubToken.value,
+                teamMembers: teamMembers.map((member) => member.githubUserName),
+                githubRepositories: githubRepositories.map((repo) => repo.path),
+            });
+
             return {
                 team,
                 teamMembers,
                 githubRepositories,
+                stats,
             };
         }),
 });
