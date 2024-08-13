4f8c615c4c9806e92e24696eefa4618d45087dae
prflow server <web@prflow.dev>
2024-08-13 04:39:07 +0000

    Tweak deploy

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ git pull origin main
 
 docker build -t prflow.dev .
 
-docker run  -p 3333:3333 -v $HOME/prisma:/app/prisma -t prflow.dev
+docker run  -p 3333:3333 -v $HOME/prisma/db.sqlite:/app/prisma/db.sqlite -t prflow.dev
