c2c8311f4a087a091637519ebccffab1905c3a8c
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2024-08-14 12:18:11 -0400

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ set -e
 
 git pull origin main
 
+docker stop $(docker ps | grep 3333 | cut -d" " -f1)
+
 docker build -t prflow.dev .
 
 docker run  -p 3333:3333 -v $HOME/prisma/:/app/hostprisma/ -t prflow.dev -d
