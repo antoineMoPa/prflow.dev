1ebd268b8e668e3f769a1bdd50736620cdd2bf5c
prflow server <web@prflow.dev>
2024-08-15 15:57:31 +0000

    tweak scripts

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,9 @@ git pull origin main
 
 docker build -t prflow.dev .
 
+docker system prune -f
+
 docker stop $(docker ps | grep 3333 | cut -d" " -f1)
 
 docker run -d -p 3333:3333 -v $HOME/prisma/:/app/hostprisma/ -t prflow.dev
+
