9fbbf3f830309053cd398268eb76adbe4d0135fa
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2024-10-07 11:49:27 -0400

    Tweak pricing

--- src/pages/pricing.tsx
+++ src/pages/pricing.tsx
@@ -10,32 +10,7 @@ export default function Team() {
                 <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem] text-center">
                     Pricing
                 </h1>
-                <p className="text-center text-xl mt-10">prflow.dev is free until we implement payment!</p>
-                <p className="text-center text-xl mt-10">The plan is to offer these tiers:</p>
-                <div className="flex w-full justify-center mt-10">
-                    <div className="flex">
-                        <div className="flex flex-col bg-[#1f1f1f] p-10 rounded-lg m-5">
-                            <h2 className="text-2xl font-bold">Basic</h2>
-                            <p className="text-lg mt-2">Track 2 metrics:</p>
-                            <ul className="list-disc mt-4 ml-4">
-                                <li>Time to first review</li>
-                                <li>Throughput</li>
-                            </ul>
-                            <p className="text-lg mt-2">2$ / developer / month</p>
-                        </div>
-                        <div className="flex flex-col bg-[#1f1f1f] p-10 rounded-lg m-5">
-                            <h2 className="text-2xl font-bold">Pro</h2>
-                            <p className="text-lg mt-2">Track all of our metrics:</p>
-                            <ul className="list-disc mt-4 ml-4">
-                                <li>Time to first review</li>
-                                <li>Throughput</li>
-                                <li>More metrics to come!</li>
-                            </ul>
-                            <p className="text-lg mt-2">5$ / developer / month</p>
-                        </div>
-                    </div>
-                </div>
-
+                <p className="text-center text-xl mt-10">200$/year for small teams (&lt;7 developers)</p>
             </main>
 
         </Layout>
