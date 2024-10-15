98d96030106164c21a1db4adc50b1a742f51fca6
Antoine Morin-Paulhus <antoine.morin.paulhus@gmail.com>
2024-10-15 09:52:12 -0400

    Tweak copy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default async function Home() {
             <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
                 <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
                     <p className="text-2xl text-center">
-                        Track PR metrics and improve your development team&apos;s efficiency with automated Slack reports.
+                        Track code metrics and improve your team&apos;s efficiency with automated Slack reports.
                     </p>
                     <div className="w-3/4 text-center">
                         <img src="slack_screenshot.png"
