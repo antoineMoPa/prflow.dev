b02cce5ea6350679f39b442ad3f7299f0acd9c2b
prflow server <web@prflow.dev>
2024-12-27 21:24:00 +0000

    improve deploy

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,10 @@ docker build -t prflow.dev .
 
 docker system prune -f
 
-docker stop $(docker ps | grep 3333 | cut -d" " -f1)
+container_id=$(docker ps | grep 3333 | cut -d" " -f1)
+if [ -n "$container_id" ]; then
+  docker stop "$container_id"
+fi
 
 docker run -d -p 3333:3333 -v $HOME/prisma/:/app/hostprisma/ -t prflow.dev
 
